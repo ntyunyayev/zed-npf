932a3a96a0208ae396dbcd97227b5b2566ad615d
Nikita Tyunyayev <nikita.tyunyayev@uclouvain.be>
2026-01-28 20:27:34 +0100

    sync query files with tree-sitter-npf
    - Update highlights.scm for new grammar structure
    - Update injections.scm with file extension detection and python_expr support
    - Add jinja template injection support

--- languages/npf/highlights.scm
+++ languages/npf/highlights.scm
@@ -1,8 +1,55 @@
-(txt_content) @string
-(role) @property
-(tag) @keyword
-(options) @keyword
-(file_name_and_extension) @keyword
-(section_name) @function
-(comment_block) @comment
+; Section headers - keywords
+"info" @keyword
+"config" @keyword
+"variables" @keyword
+"late_variables" @keyword
+"pypost" @keyword
+"pyexit" @keyword
+"script" @keyword
+"init" @keyword
+"exit" @keyword
+"require" @keyword
+"include" @keyword
+"import" @keyword
+"sendfile" @keyword
+"file" @keyword
+"init-file" @keyword
 
+; Flags
+(jinja_flag) @keyword.modifier
+(noparse_flag) @keyword.modifier
+
+; Section marker
+"%" @punctuation.special
+
+; Role
+(role "@" @operator)
+(role_name) @variable.parameter
+(multiplier) @number
+
+; Tags
+(tag "-" @operator)
+(tag (identifier) @label)
+
+; Parameters
+(param (value) @string)
+"=" @operator
+
+; Content
+(text_content) @string
+(bash_content) @embedded
+(python_content) @embedded
+(file_content) @string
+(config_content) @property
+(variables_content) @property
+
+; Inline Python expressions $(( ... ))
+(python_expr) @embedded
+
+; Filenames
+(file_basename) @string.special.path
+(file_extension) @type
+"." @punctuation.delimiter
+
+; Comments
+(comment) @comment
--- languages/npf/injections.scm
+++ languages/npf/injections.scm
@@ -1,20 +1,252 @@
-(file 
-   (file_name_and_extension
-      (_)*
-      (file_extension) @injection.language
-      (_)*
-      (_)*
-    )
-    (file_content) @injection.content)
-
+; Bash content injection
 ((bash_content) @injection.content
  (#set! injection.language "bash"))
 
-((js_line) @injection.content
- (#set! injection.combined)
- (#set! injection.language "javascript"))
-
+; Python content injection
 ((python_content) @injection.content
  (#set! injection.language "python"))
 
+; Inline Python expressions $(( ... )) - inject Python inside the expression
+((python_expr) @injection.content
+ (#set! injection.language "python"))
 
+; ============================================
+; File content injection based on extension
+; ============================================
+
+; Python files (.py)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#eq? @_ext "py")
+ (#set! injection.language "python"))
+
+; Lua files (.lua)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#eq? @_ext "lua")
+ (#set! injection.language "lua"))
+
+; JavaScript files (.js)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#eq? @_ext "js")
+ (#set! injection.language "javascript"))
+
+; TypeScript files (.ts)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#eq? @_ext "ts")
+ (#set! injection.language "typescript"))
+
+; Click configuration files (.click)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#eq? @_ext "click")
+ (#set! injection.language "click"))
+
+; Shell scripts (.sh, .bash)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#any-of? @_ext "sh" "bash")
+ (#set! injection.language "bash"))
+
+; JSON files (.json)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#eq? @_ext "json")
+ (#set! injection.language "json"))
+
+; YAML files (.yml, .yaml)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#any-of? @_ext "yml" "yaml")
+ (#set! injection.language "yaml"))
+
+; TOML files (.toml)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#eq? @_ext "toml")
+ (#set! injection.language "toml"))
+
+; XML files (.xml)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#eq? @_ext "xml")
+ (#set! injection.language "xml"))
+
+; HTML files (.html, .htm)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#any-of? @_ext "html" "htm")
+ (#set! injection.language "html"))
+
+; CSS files (.css)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#eq? @_ext "css")
+ (#set! injection.language "css"))
+
+; C files (.c, .h)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#any-of? @_ext "c" "h")
+ (#set! injection.language "c"))
+
+; C++ files (.cpp, .hpp, .cc, .cxx)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#any-of? @_ext "cpp" "hpp" "cc" "cxx")
+ (#set! injection.language "cpp"))
+
+; Rust files (.rs)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#eq? @_ext "rs")
+ (#set! injection.language "rust"))
+
+; Go files (.go)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#eq? @_ext "go")
+ (#set! injection.language "go"))
+
+; Ruby files (.rb)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#eq? @_ext "rb")
+ (#set! injection.language "ruby"))
+
+; Perl files (.pl, .pm)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#any-of? @_ext "pl" "pm")
+ (#set! injection.language "perl"))
+
+; SQL files (.sql)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#eq? @_ext "sql")
+ (#set! injection.language "sql"))
+
+; Makefile
+((file_section
+  (file_header
+    (filename
+      name: (file_basename) @_name))
+  (file_content) @injection.content)
+ (#eq? @_name "Makefile")
+ (#set! injection.language "make"))
+
+; Dockerfile
+((file_section
+  (file_header
+    (filename
+      name: (file_basename) @_name))
+  (file_content) @injection.content)
+ (#eq? @_name "Dockerfile")
+ (#set! injection.language "dockerfile"))
+
+; Config/text files (.txt, .cfg, .conf, .ini)
+((file_section
+  (file_header
+    (filename
+      extension: (file_extension) @_ext))
+  (file_content) @injection.content)
+ (#any-of? @_ext "txt" "cfg" "conf" "ini")
+ (#set! injection.language "ini"))
+
+; ============================================
+; Jinja templates (when jinja flag is set)
+; ============================================
+
+; Script with jinja flag
+((script_section
+  (script_header
+    (jinja_flag))
+  (bash_content) @injection.content)
+ (#set! injection.language "jinja"))
+
+; Init with jinja flag
+((init_section
+  (init_header
+    (jinja_flag))
+  (bash_content) @injection.content)
+ (#set! injection.language "jinja"))
+
+; Exit with jinja flag
+((exit_section
+  (exit_header
+    (jinja_flag))
+  (bash_content) @injection.content)
+ (#set! injection.language "jinja"))
+
+; Require with jinja flag
+((require_section
+  (require_header
+    (jinja_flag))
+  (bash_content) @injection.content)
+ (#set! injection.language "jinja"))
+
+; File with jinja flag
+((file_section
+  (file_header
+    (jinja_flag))
+  (file_content) @injection.content)
+ (#set! injection.language "jinja"))
